3ed37ffe0281cd98c39e37ee36733fec341a764b
Pedro <xelespedro@gmail.com>
2025-07-04 15:30:44 -0300

    ds

--- routers/users.py
+++ routers/users.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from models import Users
+from models import User
 import schemas
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", response_model=schemas.User)
 def criar_user(item: schemas.UserCreate, db: Session = Depends(get_db)):
-    novo_user = Users(**item.dict())
+    novo_user = User(**item.dict())  # <- corrigido aqui
     db.add(novo_user)
     db.commit()
     db.refresh(novo_user)
