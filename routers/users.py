d8d6486c762a310a1a982b639e54cbba498fd623
Pedro <xelespedro@gmail.com>
2025-07-04 15:50:03 -0300

    ds

--- routers/users.py
+++ routers/users.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", response_model=schemas.User)
 def criar_user(item: schemas.UserCreate, db: Session = Depends(get_db)):
-    novo_user = models.User(**item.dict())
+    novo_user = models.User(**item.dict(exclude_unset=True))
     db.add(novo_user)
     db.commit()
     db.refresh(novo_user)
