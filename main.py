81643c1cad599dacac9ceceddd107a7edd7cf15e
Pedro <xelespedro@gmail.com>
2025-07-04 10:10:32 -0300

    ds

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Rota principal para testes (GET /)
+@app.get("/")
+def read_root():
+    return {"mensagem": "API online com sucesso!"}
+
+# Suas rotas já existentes
 app.include_router(mercado_financeiro.router)
