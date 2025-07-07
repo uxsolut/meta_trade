160d5fd2cbda3dce1367d76e5fa10d29980ac683
Pedro <xelespedro@gmail.com>
2025-07-07 14:34:41 -0300

    ds

--- main.py
+++ main.py
@@ -16,3 +16,9 @@ app.include_router(robos.router)
 app.include_router(users.router)          
 app.include_router(robos_do_user.router)  
 app.include_router(requisicoes.router)
+
+import os
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
