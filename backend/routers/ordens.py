f88611210d6b2a550d785edad23f1a1cb73bec52
Pedro <xelespedro@gmail.com>
2025-07-16 15:31:12 -0300

    ds

--- backend/routers/ordens.py
+++ backend/routers/ordens.py
@@ -7,16 +7,16 @@ from models.users import User
 from schemas.ordens import OrdemCreate, Ordem as OrdemSchema
 from auth.dependencies import get_db, get_current_user
 
-
 router = APIRouter(prefix="/ordens", tags=["Ordem"])
 
 @router.post("/", response_model=OrdemSchema)
 def criar_ordem(
     item: OrdemCreate,
     db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user),  # Proteção via JWT
+    current_user: User = Depends(get_current_user),
 ):
     nova_ordem = Ordem(**item.dict(exclude_unset=True))
+    nova_ordem.id_user = current_user.id  # 👉 Garante que a ordem pertence ao usuário logado
     db.add(nova_ordem)
     db.commit()
     db.refresh(nova_ordem)
@@ -25,6 +25,6 @@ def criar_ordem(
 @router.get("/", response_model=List[OrdemSchema])
 def listar_ordens(
     db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user),  # Proteção via JWT
+    current_user: User = Depends(get_current_user),
 ):
-    return db.query(Ordem).all()
+    return db.query(Ordem).filter(Ordem.id_user == current_user.id).all()  # 👉 Filtro aqui
