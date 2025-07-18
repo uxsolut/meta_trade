c320a1cfaf481c39e45d63e45345bb6c60523917
Pedro <xelespedro@gmail.com>
2025-07-18 12:37:42 -0300

    ds

--- backend/models/contas.py
+++ backend/models/contas.py
@@ -6,17 +6,16 @@ class Conta(Base):
     __tablename__ = "contas"
 
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, nullable=True)  # Novo campo que você adicionou no banco
+    nome = Column(String, nullable=True)
     id_robo_user = Column(Integer, ForeignKey("robos_do_user.id", ondelete="CASCADE"))
     conta_meta_trader = Column(String, nullable=True)
     id_corretora = Column(Integer, ForeignKey("corretoras.id", ondelete="SET NULL"))
 
     corretora = relationship("Corretora", back_populates="contas")
     users = relationship("User", backref="conta")
-    carteiras = relationship("Carteira", back_populates="conta")
 
     robo_user = relationship(
-      "RobosDoUser",
-      back_populates="conta",  # deve bater com o nome no outro lado
-      foreign_keys="[RobosDoUser.id_conta]"  # define explicitamente a FK
+        "RobosDoUser",
+        back_populates="conta",
+        foreign_keys="[RobosDoUser.id_conta]"
     )
