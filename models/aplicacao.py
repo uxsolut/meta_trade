7e995a205cf860a63e4a768d5b65035c9a63f3c2
Pedro <xelespedro@gmail.com>
2025-07-25 09:24:31 -0300

    ds

--- models/aplicacao.py
+++ models/aplicacao.py
@@ -1,12 +1,15 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.dialects.postgresql import ENUM
 from database import Base
+
+tipo_aplicacao_enum = ENUM('backend', 'frontend', name='tipo_aplicacao', create_type=False)
 
 class Aplicacao(Base):
     __tablename__ = "aplicacao"
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(255), nullable=False)
-    tipo = Column(String, nullable=False)
+    tipo = Column(tipo_aplicacao_enum, nullable=False)
     id_versao_aplicacao = Column(Integer, ForeignKey("versao_aplicacao.id"), nullable=False)
     criado_em = Column(DateTime, server_default=func.current_timestamp())
     atualizado_em = Column(DateTime, server_default=func.current_timestamp(), onupdate=func.current_timestamp())
